b474a88c40a40d140af04db6fbf637b6ff67f01b
sakir456 <shaikhsakir1015@gmail.com>
2024-02-09 19:05:09 +0530

    class component

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,36 @@
-const About = () => {
-    return (
-        <div>
-        <h1>About</h1>
-        <h2>This is food ordering App By sakir shaikh</h2>
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
 
 
-        </div>
-    );
-};
+
+class About extends Component{
+
+    constructor(props){
+        super(props)
+
+        console.log("parent constructor")
+    }
+
+    componentDidMount(){
+        console.log("parent component did mount")
+     }
+
+
+    render(){
+        console.log("parent render")
+        return (
+            <div>
+            <h1>About</h1>
+            <h2>This is food ordering App By sakir shaikh</h2>
+            <UserClass name={"Sakir Shaikh (class)"} location={"Bharuch (class)"} />
+            
+            </div>
+        );
+    }
+}
+
+
+
 
 export default About
---
+++ src/components/UserClass.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+class UserClass extends React.Component {
+ constructor(props) {
+        super(props);
+      
+        this.state = {
+            userInfo: {
+            name:"dummy",
+            location:"default",
+            },
+        };
+       
+
+    }
+
+     async componentDidMount(){
+        const data=await fetch("https://api.github.com/users/sakir456");
+        const json=await data.json();
+
+        this.setState({
+            userInfo:json,
+        });
+        console.log(json)
+     }
+
+     componentDidUpdate(){
+
+     }
+
+     componentWillUnmount(){
+        
+     }
+    
+     render(){
+         const{ name, location, avatar_url } =this.state.userInfo;
+       
+        return(
+            <div className="user-card">
+                <img src={avatar_url}/>
+             <h2>Name: {name}</h2>
+             <h3>Location: {location}</h3>
+             <h4>Contact:@sakir456</h4>
+            </div>
+
+        );
+    }
+}
+export default UserClass
